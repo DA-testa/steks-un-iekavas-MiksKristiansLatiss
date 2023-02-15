fa7845afab3d5fd019b7dc2b7c8c4690f728c431
MiksKristiansLatiss <125253937+MiksKristiansLatiss@users.noreply.github.com>
2023-02-15 18:01:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,18 +19,21 @@ def find_mismatch(text):
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            if not opening_brackets_stack or not opening_brackets_stack[-1].match(next):
-                print(i + 1)
-                return
-            opening_bracekts_stack.pop()
-            pass
-
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
+                return i + 1
+            opening_brackets_stack.pop()
+    if opening_brackets_stack:
+      return opening_brackets_stack[0].position
+    return "Success"
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
-
+    if mismatch == "Success":
+      print("Success")
+    else:
+      print(mismatch)
 
 if __name__ == "__main__":
     main()
